4ebd67b4307feb17c34d8ae985aa8ed3c8a8cce0
Joseph Shetaye <joseph@shetaye.me>
2025-05-21 19:21:37 -0700

    FIXED THE NETWORK ISSUE

--- webserver.py
+++ webserver.py
@@ -1,6 +1,7 @@
 import base64
 import io
 import json
+import time
 from fastapi import FastAPI, Request
 from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.responses import StreamingResponse
@@ -22,7 +23,8 @@ CHECK_WORK = True
 model_cache = ModelCache()
 # pipeline, g = tests.test_safetensor()
 # pipeline, g = tests.test_llama_layernorm()
-pipeline, g = tests.test_pipelining()
+#pipeline, g = tests.test_pipelining()
+pipeline, g = tests.test_llama_attn()
 worker_manager = WorkerManager(g)
 
 app = FastAPI()
@@ -38,8 +40,6 @@ app = FastAPI()
 
 inflight_work = {}
 sim_results = {}
-
-app.add_middleware(GZipMiddleware, minimum_size=1000)  # Compress responses larger than 1KB
 
 @app.post("/register", response_model=Registration)
 async def register():
@@ -66,6 +66,13 @@ class SafetensorHeader(BaseModel):
     dtype: str
     shape: list[int]
 
+async def stream_bytes(bytes: bytes, chunk_size: int = 1024 * 500):
+    n_bytes = len(bytes)
+    for i in range(0, n_bytes, chunk_size):
+        start = i
+        end = min(i + chunk_size, n_bytes)
+        yield bytes[start:end]
+
 @app.get("/safetensor/{model_name}/{tensor_name}")
 async def get_safetensor(model_name: str, tensor_name: str):
     """
@@ -90,9 +97,18 @@ async def get_safetensor(model_name: str, tensor_name: str):
         elif tensor.dtype == torch.float16:
             tensor = tensor.to(torch.float32)
         tensor = Tensor.from_torch(tensor)
-        bytes = bytearray(size_encoded_tensor(tensor))
-        write_encoded_tensor(bytes, 0, tensor)
-        return StreamingResponse(io.BytesIO(bytes), media_type="application/octet-stream")
+    tensorBytes = bytearray(size_encoded_tensor(tensor))
+    write_encoded_tensor(tensorBytes, 0, tensor)
+    tensorBytes = bytes(tensorBytes)
+
+    # For large files, use chunked transfer encoding
+    return StreamingResponse(
+        stream_bytes(tensorBytes),  # Send the entire buffer as one chunk
+        media_type="application/octet-stream",
+        headers={
+            "Content-Length": str(len(tensorBytes)),
+        }
+    )
 
 @app.get("/work/{partition_name}")
 async def get_work(partition_name: PartitionName):
@@ -142,4 +158,4 @@ app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", workers=2, port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
